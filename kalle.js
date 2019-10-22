c033a251632d163be57db35c9b75f2a4f7199a14
kalle <carl.hammar@gmail.com>
2019-10-22 14:24:52 +0200

    anime

---
+++ kalle.js
@@ -0,0 +1,22 @@
+function clickHandler(){
+    anime({
+       targets: '.box',
+        translateX: 250
+  });
+}
+
+let form = document.querySelector(".box")
+form.addEventListener("click",clickHandler)
+
+let buttons = document.querySelector("button")
+button.addEventListener("click", event =>{
+   for(button of buttons){
+       
+   }
+
+
+    window.location = event
+    .currentTarget
+    .getAttribute("href")
+
+})
